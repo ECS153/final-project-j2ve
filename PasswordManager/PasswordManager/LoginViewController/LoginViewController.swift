7285e4bf264ef13702c512c3110b19672bc8e860
Jenna Zing <jszing@ucdavis.edu>
2020-05-25 15:37:41 -0700

    working Login Navigation flow

--- PasswordManager/PasswordManager/LoginViewController/LoginViewController.swift
+++ PasswordManager/PasswordManager/LoginViewController/LoginViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import Foundation
+import UIKit
 
-class loginPage: UIViewController {
+class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        // self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
 }
--- PasswordManager/PasswordManager/ViewController.swift
+++ PasswordManager/PasswordManager/ViewController.swift
@@ -2,10 +2,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // override func viewDidLoad() {
+        // super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+    // }
 
 
 }
