75f2eba3d7b3a6cba8fb59fa1866543a123d6ff6
VivZing <vazing@ucdavis.edu>
2020-05-25 14:57:34 -0700

    update to login navigation controller

--- PasswordManager/PasswordManager/LoginViewController/loginPage.swift
+++ PasswordManager/PasswordManager/LoginViewController/loginPage.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+class loginPage: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
