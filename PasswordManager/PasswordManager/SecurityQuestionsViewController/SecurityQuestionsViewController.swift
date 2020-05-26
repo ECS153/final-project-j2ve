07b72c8e54777584efe4cd6713473eaf9d2e8ce3
VivZing <vazing@ucdavis.edu>
2020-05-25 19:03:28 -0700

    Revert "security question seguing"
    This reverts commit 7667013c45f461088a33c8fe8533685107e59993.

--- PasswordManager/PasswordManager/SecurityQuestionsViewController/SecurityQuestionsViewController.swift
+++ PasswordManager/PasswordManager/SecurityQuestionsViewController/SecurityQuestionsViewController.swift
@@ -10,24 +10,7 @@ import UIKit
 import Foundation
 
 class SecurityQuestionsViewController: UIViewController {
-    
-    @IBOutlet weak var submitButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    @IBAction func submitButtonPress(_ sender: Any) {
-        // get entered answers  + check if answers are correct / valid
-        
-        // if answers entered are correct, use the following code:
-        
-        // casting view controller as registered accounts view controller
-        let registeredAccountController = UIStoryboard(name: "RegisteredAccountsView", bundle: nil).instantiateViewController(withIdentifier: "RegisteredAccountsViewController") as! RegisteredAccountsViewController
-        
-        // get navigation controller so can create a new flow of the app (login flow has finished)
-        let navigationController = UINavigationController(rootViewController: registeredAccountController)
-        navigationController.modalPresentationStyle = .fullScreen
-        self.present(navigationController, animated: true)
     }
 }
