84922653593d572bb7d88a00d6962076bfbf1475
VivZing <vazing@ucdavis.edu>
2020-05-25 19:03:09 -0700

    Revert "registered account logout button segue"
    This reverts commit 3fd2873bac615ea573f78252e6f85a42c51cbf09.

--- PasswordManager/PasswordManager/RegisteredAccountsViewController/RegisteredAccountsViewController.swift
+++ PasswordManager/PasswordManager/RegisteredAccountsViewController/RegisteredAccountsViewController.swift
@@ -10,28 +10,7 @@ import UIKit
 import Foundation
 
 class RegisteredAccountsViewController: UIViewController {
-    
-    @IBOutlet weak var addAccountButton: UIButton!
-    @IBOutlet weak var logoutButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    @IBAction func addAccountButtonPress(_ sender: Any) {
-        // segue over to add account screen
-    }
-    
-    
-    @IBAction func logoutButtonPress(_ sender: Any) {
-        // go back to login screen
-        
-        // casting view controller as login view controller
-        let loginController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
-        
-        // get navigation controller so can create a new flow of the app
-        let navigationController = UINavigationController(rootViewController: loginController)
-        navigationController.modalPresentationStyle = .fullScreen
-        self.present(navigationController, animated: true)
     }
 }
