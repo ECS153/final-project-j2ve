f93370c005ba79560ad6f93c2ea85dd336eb2a66
Evelyn Sjafii <evelynsj98@gmail.com>
2020-05-26 20:41:30 -0700

    Login handles errors and functionality is completed for now

--- PasswordManager/PasswordManager/LoginViewController/LoginViewController.swift
+++ PasswordManager/PasswordManager/LoginViewController/LoginViewController.swift
@@ -8,33 +8,83 @@
 
 import UIKit
 import Foundation
+import Firebase
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var masterEmailTextField: UITextField!
+    @IBOutlet weak var masterPasswordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var errorMessage: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        // hide error message label upon loading
+        errorMessage.alpha = 0
+        print("View did load")
+    }
+    
+    func showErrorMessage(_ message: String!) {
+        errorMessage.text = message
+        errorMessage.alpha = 1
+    }
+    
+    func validateInput(_ masterEmail: String?,_ masterPassword: String?) -> String? {
+        // Return error message if email or password is empty
+        if masterEmail == "" {
+            return "Please enter master email"
+        }
+        if masterPassword == "" {
+            return "Please enter master password"
+        }
+
+        // Else return nil
+        return nil
+        
     }
 
     @IBAction func loginButtonPress(_ sender: Any) {
-        // get entered password info + check if password is correct / valid
+        // sanitize email and master password
+        let masterEmail = masterEmailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let masterPassword = masterPasswordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        
-        // if password entered is correct, use the following code:
-        
-        // navigate (segue over) to the registered accounts screen
-        let storyboard = UIStoryboard(name: "SecurityQuestions", bundle: nil)
-        
-        // casting view controller as a registered accounts view controller
-        // forced unwrapping (!):
-        // using it here b/c we potentially want to set some variables on that object (navigation has to use downcast)
-        // if it's not a registered accounts view controller, then what do you do?  Should just crash the app
-        let viewController = storyboard.instantiateViewController(withIdentifier: "SecurityQuestionsViewController") as! SecurityQuestionsViewController
-        
-        self.navigationController?.pushViewController(viewController, animated: true)
+        // validate inputs
+        let error = validateInput(masterEmail, masterPassword)
+        if error != nil {
+            showErrorMessage(error)
+            return
+        }
+
+        // sign user in
+        Auth.auth().signIn(withEmail: masterEmail!, password: masterPassword!) { (result, error) in
+            // If there is an error with signing user in, show error message
+            if (error != nil) {
+                let errCode = AuthErrorCode(rawValue: error!._code)
+                // TODO: There is probably a more elegant way to do this
+                if (errCode?.rawValue == 17008) {
+                    self.showErrorMessage("Please enter correctly formatted email")
+                }
+                else if (errCode?.rawValue == 17011 || errCode?.rawValue == 17009) {
+                    self.showErrorMessage("Your account does not exist or password is invalid") // registered?
+                }
+                return
+            }
+            // User successfully signed in, segue to the security questions screen
+            else {
+                let storyboard = UIStoryboard(name: "SecurityQuestions", bundle: nil)
+                
+                // casting view controller as a registered accounts view controller
+                // forced unwrapping (!):
+                // using it here b/c we potentially want to set some variables on that object (navigation has to use downcast)
+                // if it's not a registered accounts view controller, then what do you do?  Should just crash the app
+                let viewController = storyboard.instantiateViewController(withIdentifier: "SecurityQuestionsViewController") as! SecurityQuestionsViewController
+                
+                self.navigationController?.pushViewController(viewController, animated: true)
+            }
+        }
     }
     
     
