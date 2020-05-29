f95fba26adf73dd596cb58c44aed8b025ca5f17c
Evelyn Sjafii <evelynsj98@gmail.com>
2020-05-28 18:58:55 -0700

    Commit new files for AddRegisteredAccount

---
+++ PasswordManager/PasswordManager/AddRegisteredAccountViewController/AddRegisteredAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddRegisteredAccountViewController.swift
+//  PasswordManager
+//
+//  Created by Evelyn Sjafii on 5/28/20.
+//  Copyright © 2020 Vivienne Zing. All rights reserved.
+//
+
+import UIKit
+
+class AddRegisteredAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
